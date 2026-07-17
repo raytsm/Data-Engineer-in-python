b064a33fa74556d49d0e91be42e714bdf3ba30b2
Roshan Thapa <roshanrays236@gmail.com>
2026-07-17 12:33:23 +0545

    WIP

---
+++ CodeFiles/IntroToPythonForDevelopers.py
@@ -0,0 +1,19 @@
+print("Hello, world!")
+# Hello, world!
+print('Hello, world!')
+# Hello, world!
+
+# This is a code comment
+# The below is a print function
+print("Hello, world!")
+
+ingredient_name = "Tomatoes"
+ingredient_quantity = 2
+
+print(ingredient_name)
+# Tomatoes
+print(ingredient_quantity)
+# 2
+ingredient_quantity = 1
+print(ingredient_quantity)
+# 1
